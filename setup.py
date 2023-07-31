6d0faac42bcb12d6e0358c5860826535a4f45fad
re4mfy <re4mfy@virginia.edu>
2023-07-31 10:54:19 -0400

    made necessary files to make files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TestPackage',
+    version='1.0.0',
+    url='https://github.com/rteb8/test_packg',
+    author='Rose E.M.',
+    author_email='re4mfy@virginia.edu',
+    description='Testing how to make a package by building the Joke package',
+    packages=find_packages()
+)
---
+++ testpack_dir/__init__.py
@@ -0,0 +1,2 @@
+print("Welcome to the Joke package, this where I learned \
+      how to make a package using a Monty Python joke")
